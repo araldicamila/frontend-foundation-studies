7a38514a38e6bf96ee22661a63e7a21adc225753
Camila Araldi <araldi.cami@gmail.com>
2021-10-27 21:28:28 -0300

    Datalist and primitive values

---
+++ javascript/primitiveValues.js
@@ -0,0 +1,16 @@
+// Primitive values
+const typeString = "Hello, I'm a String!";
+
+const typeNumbers = 1.5;
+
+const typeBigint = 9007199254740991n;
+
+const typeBoolean = true;
+
+const negativeBoolean = false;
+
+const typeNull = null;
+
+typeof null; // BUG 🐞 🐞 🐞
+
+const typeUndefined = undefined;
